60395cb773c65fc63b67520230ff9e17b131d534
lxfly2000 <gaoboyuan8@qq.com>
2017-06-23 16:59:20 +0800

    实现 Ch1-9 的转换功能。

--- Readme.md
+++ Readme.md
@@ -8,8 +8,7 @@ PMD 到 MIDI 的转换工具。
 请用 VS2017 编译，本程序需要 [midifile](https://github.com/craigsapp/midifile) 和 [pmdplay](https://github.com/lxfly2000/pmdplay) 来工作，可通过运行 `fetchlib.ps1` 获得。
 
 ### 提示
-使用本程序前请先在程序目录中创建一个 `patch.txt` 文件，在里面写上 PMD 到 GM 的音色转换，一共 256 行，每行写对应的 GM
-音色号即可。
+使用本程序前请先在程序目录中创建一个 `patch.txt` 文件，在里面写上 PMD 到 GM 的音色转换，一共 256 行，每行写对应的 GM 音色号即可。
 
 ## TODO
 该程序目前正在制作中。
--- pmdconv.cpp
+++ pmdconv.cpp
@@ -13,11 +13,11 @@
 #include<iostream>
 #include<fstream>
 #define MAX_PMD_PATCH 256
-int patch[MAX_PMD_PATCH];
+int g_patch[MAX_PMD_PATCH];
 void InitPatch()
 {
 	std::ifstream pf("patch.txt", std::ios::in);
-	for (int i = 0; i < MAX_PMD_PATCH; i++)patch[i] = i % 128;
+	for (int i = 0; i < MAX_PMD_PATCH; i++)g_patch[i] = i % 128;
 	if (!pf)
 	{
 		printf("未找到 patch.txt, 程序将以PMD音色作为GM音色处理。"
@@ -28,23 +28,149 @@ void InitPatch()
 		int i = 0;
 		while (i < MAX_PMD_PATCH)
 		{
-			pf >> patch[i];
-			if (patch[i] > 127)printf("音色[%d]=%d, 超出最大值 127.\n", i, patch[i]);
+			pf >> g_patch[i];
+			if (g_patch[i] > 127)printf("音色[%d]=%d, 超出最大值 127.\n", i, g_patch[i]);
+			i++;
 			if (pf.eof())break;
 		}
 		printf("读取了%d个音色。\n", i);
 	}
 }
+struct ChannelNote
+{
+	int note = 0, last_note = 0;
+	int volume = 0;//FM最大127，SSG最大15
+	int patch = 0;
+	int ch = 0;
+	bool keyison = false;
+	void UpdateChannel()
+	{
+		volume = getpartwork(ch)->volume;
+		last_patch = patch;
+		patch = getpartwork(ch)->voicenum;
+		last_note = note;
+		note = (getpartwork(ch)->onkai & 0xF) + ((getpartwork(ch)->onkai >> 4) * 12);
+		last_key_on_count = key_on_count;
+		key_on_count = getpartwork(ch)->keyon_flag;
+	}
+	bool IsOnNoteOn()
+	{
+		return (key_on_count != last_key_on_count) || (note < 128 && last_note >= 128);
+	}
+	bool IsOnNoteOff()
+	{
+		return (getpartwork(ch)->keyoff_flag != 0) || (note >= 128 && last_note < 128);
+	}
+	bool IsOnProgramChange()
+	{
+		return last_patch != patch;
+	}
+	int GetVolumeInGM()
+	{
+		if (ch < 6)return volume;
+		else return 127 * volume / 15;
+	}
+private:
+	int last_key_on_count = 0;
+	int last_patch = 0;
+	int key_on_count = 0;
+};
+struct ChannelRhythm
+{
+	int notes[128] = { 0 };
+	int volume = 0;//最大63
+	void UpdateChannel()
+	{
+		volume = getopenwork()->rhyvol;
+	}
+};
+struct Channels
+{
+	ChannelNote chnote[9];
+	ChannelRhythm chrhy;
+	Channels()
+	{
+		for (int i = 0; i < 9; i++)chnote[i].ch = i;
+	}
+	void Update()
+	{
+		for (int i = 0; i < 9; i++)chnote[i].UpdateChannel();
+		last_tempo = tempo;
+		tempo = getopenwork()->tempo_48 * 2;
+	}
+	int tempo = 120;
+	bool IsTempoChanged()
+	{
+		return last_tempo != tempo;
+	}
+private:
+	int last_tempo = 120;
+};
 int Convert(const char *infile, const char *outfile)
 {
 	pmdwininit(".");
+	setppsuse(false);
+	setrhythmwithssgeffect(true);
+	getopenwork()->effflag = 0;
+	setfmcalc55k(true);
 	setpcmrate(SOUND_44K);
-	const int buf_length = 8192;
-	short buf[buf_length];
-	//TODO
-	//普通音符直接获取每通道QQ中的信息即可，节奏部分采用R通道音符，可以获取到音量，Tom需要参考K通道鼓点。
-	printf("TODO:程序正在制作中。\n如需获取程序更新，请关注 https://github.com/lxfly2000/pmdconv.");
+	char musicfilepath[_MAX_PATH];
+	strcpy(musicfilepath, infile);
+	if (music_load(musicfilepath))
+	{
+		printf("加载文件 %s 时出错，请检查该文件是否是有效的 PMD 文件。\n", musicfilepath);
+		return -1;
+	}
+	music_start();
+	int tpq = getopenwork()->syousetu_lng / 4;
+	MidiFile mf;
+	Channels pmdch;
+	int bytesof_buf = SOUND_44K * 2 * 2;//一秒音频的内存长度
+	char *renbuf = new char[bytesof_buf];
+	mf.addTrack(10);
+	mf.setTicksPerQuarterNote(tpq);
+	for (int i = 0; i < 9; i++)
+	{
+		char tname[32];
+		sprintf(tname, "PMD Ch %c", 'A' + i);
+		mf.addTrackName(i, 0, tname);
+	}
+	int nowtick = getpos2();
+	int lengthtick = 0, looplengthtick = 0;
+	getlength2(musicfilepath, &lengthtick, &looplengthtick);
+	for (int i = 6; i <= 8; i++)mf.addPatchChange(i + 1, 0, i, 80);
+	while (nowtick < lengthtick)
+	{
+		pmdch.Update();
+		if (pmdch.IsTempoChanged())mf.addTempo(0, nowtick, pmdch.tempo);
+		for (int i = 0; i < 9; i++)
+		{
+			if (pmdch.chnote[i].IsOnNoteOn())
+			{
+				if (pmdch.chnote[i].keyison)mf.addNoteOff(i + 1, nowtick, i, pmdch.chnote[i].last_note);
+				pmdch.chnote[i].keyison = true;
+				if (pmdch.chnote[i].note < 128)
+					mf.addNoteOn(i + 1, nowtick, i, pmdch.chnote[i].note, pmdch.chnote[i].GetVolumeInGM());
+			}
+			if (pmdch.chnote[i].IsOnNoteOff())
+			{
+				pmdch.chnote[i].keyison = false;
+				if (pmdch.chnote[i].last_note < 128)
+					mf.addNoteOff(i + 1, nowtick, i, pmdch.chnote[i].last_note);
+			}
+			if (pmdch.chnote[i].IsOnProgramChange())mf.addPatchChange(i + 1, nowtick, i, g_patch[pmdch.chnote[i].patch]);
+		}
+		getpcmdata((short*)renbuf, min(SOUND_44K, SOUND_44K * 60 / tpq / pmdch.tempo));
+		nowtick = getpos2();
+	}
+	for (int i = 0; i < 9; i++)
+		if (pmdch.chnote[i].keyison)
+			mf.addNoteOff(i + 1, nowtick, i, pmdch.chnote[i].note);
+	//TODO:实现节奏通道转换
+	delete[]renbuf;
 	music_stop();
+	mf.sortTracks();
+	mf.write(outfile);
 	return 0;
 }
 int main(int argc, char *argv[])
